f4512cfebcab57b81ff0c6c86bd99fbaa9771fcc
Yonayv <fixicoyonayv@gmail.com>
2023-04-11 14:44:31 -0500

    Weehoo

---
+++ LoopsProjectYF/LoopsProjectYF/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoopsProjectYF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int CountNumberOfSpaces(string sentence)
+        {
+            int numberOfSpaces = 0;
+            foreach(char character in sentence)
+            {
+                if(character == ' ')
+                {
+                    numberOfSpaces++;
+                }
+            }
+            return numberOfSpaces;
+
+
+        }
+    }
+}
